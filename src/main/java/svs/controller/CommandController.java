30238c15d974d165b935eb2a7b69c1100c1fe5a9
Vladislav Surkov <vladucha09@inbox.ru>
2025-07-21 06:01:57 +0300

    feat: попытка в прометей и новый эндпоинт в контроллере

--- src/main/java/svs/controller/CommandController.java
+++ src/main/java/svs/controller/CommandController.java
@@ -7,8 +7,11 @@ import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
 import svs.command.dto.CommandDto;
+import svs.command.dto.CommandStatsDto;
 import svs.command.mapper.CommandMapper;
+import svs.command.mapper.CommandStatsMapper;
 import svs.command.service.CommandService;
+import svs.metrics.MetricsService;
 
 @RestController
 @RequestMapping("/commands")
@@ -17,11 +20,19 @@ import svs.command.service.CommandService;
 public class CommandController {
 
     private final CommandService commandService;
+    private final MetricsService metricsService;
 
     @PostMapping
     public ResponseEntity<String> submitCommand(@Valid @RequestBody CommandDto commandDto) {
         log.info("submitCommand received");
         commandService.executeCommand(CommandMapper.toCommand(commandDto));
         return ResponseEntity.ok("Command accepted");
+    }
+
+    @GetMapping("/stats")
+    public ResponseEntity<CommandStatsDto> getStats() {
+        CommandStatsDto dto = CommandStatsMapper.toCommandStatsDto(metricsService.getTotalCommands(),
+                metricsService.getUptimeSeconds());
+        return ResponseEntity.ok(dto);
     }
 }
